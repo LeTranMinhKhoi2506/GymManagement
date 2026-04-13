91576724db7b86c4b3bd2782ae1271655695089b
Toshiro19-w <6451071036@st.utc2.edu.vn>
2026-04-13 21:50:10 +0700

    init login and register

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,31 @@
+class UserModel {
+  final String uid;
+  final String email;
+  final String fullName;
+  final String role; // 'admin' or 'user'
+
+  UserModel({
+    required this.uid,
+    required this.email,
+    required this.fullName,
+    required this.role,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'email': email,
+      'fullName': fullName,
+      'role': role,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      uid: map['uid'] ?? '',
+      email: map['email'] ?? '',
+      fullName: map['fullName'] ?? '',
+      role: map['role'] ?? 'user',
+    );
+  }
+}
